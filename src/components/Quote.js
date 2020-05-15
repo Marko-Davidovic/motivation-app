31e369cc8214fff9a9172dbfb1e5b1fcbfb27d08
Marko Davidovic <davidovic-m@hotmail.com>
2020-05-15 01:01:01 +0000

    first commit

---
+++ src/components/Quote.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Quote = () => {
+
+    return (
+        <div id="qoute-box">
+        <div>Quote</div>
+        <div id="text"><p>Lorem ansgiasg sabgouasbgs gsd hdjskhds. dj gsdjbgjkd. gasbjgsjakb nkangsub sjgbabsgbasj jfgasjbg</p></div>
+        <div id="author">Marko Davidovic</div>
+        <button id="button">New Quote</button>
+        </div>
+    )
+}
+export default Quote ;
